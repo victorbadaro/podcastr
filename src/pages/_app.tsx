1a94f4b4d4de9f6b796ecb3b6057a83817424ca9
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-04-22 23:52:01 -0300

    feat: add Player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,42 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
 
-      <Player />
-    </div>
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  return (
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
