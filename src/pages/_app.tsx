d3963a8c20b046b51472e868ffba7e2ec2e46b73
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-04-23 23:55:42 -0300

    chore: change player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,14 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
-import { PlayerContext } from '../contexts/PlayerContext';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 
 import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
-import { useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
-
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+    <PlayerContextProvider>
       <div className={styles.wrapper}>
         <main>
           <Header />
@@ -36,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         <Player />
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   );
 }
 
